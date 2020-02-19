448ae911ad9662cf3282f665f05487f04f4798fa
bcrazydreamer <31788510+bcrazydreamer@users.noreply.github.com>
2020-02-19 15:12:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,9 +45,14 @@ app.options("*",function(req,res,next){
   res.status(200).end();
 });
 
-// app.get('/', async function(req, res, next) {
-//   return res.status(200).json({status : true});
-// })
+app.get('/', async function(req, res, next) {
+  var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  console.log(ip);
+  return res.status(200).json({status : true,ip : ip});
+})
 
 app.post('/', async function(req, res, next) {
   var data = req.body;
